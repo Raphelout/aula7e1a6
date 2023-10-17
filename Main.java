e63b258e16c91978bc3c16fdf6bc4f49f8dab65b
Raphael Lout <145383096+Raphelout@users.noreply.github.com>
2023-10-17 17:51:15 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
-    
+
     public static void main(String[] args){
+
         
         Conta conta1 = new Conta();
         conta1.saldo = 10;
         conta1.atualiza(1.5);
         System.out.println(conta1.saldo);
-        
     }
 
 }
