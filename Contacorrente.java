e5d078008dbc383d1b416a014f4bc8ae7cd6fd1d
Raphael Lout <145383096+Raphelout@users.noreply.github.com>
2023-10-17 17:50:40 -0300

    Update Contacorrente.java

--- Contacorrente.java
+++ Contacorrente.java
@@ -1,3 +1,16 @@
-public class Contacorrente {
-    
+import java.util.Scanner;
+
+public class Contacorrente extends Conta{
+
+    public void atualiza(double taxa){
+        this.saldo += this.saldo * taxa * 2;
+
+    }
+    public void deposita(){
+            Scanner Leitor = new Scanner(System.in);
+            System.out.println("digite o valor que quer depositar");
+            double quantdpo = Leitor.nextDouble();
+            this.saldo += quantdpo - 0.10;
+            Leitor.close();
+    }
 }
